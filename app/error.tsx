b2a9ea439e48a4a2fa860d407ed679043e6ee9b1
Njenga Muriu <smuriu@gmail.com>
2024-04-20 12:45:24 +0300

    adds basic error layout

---
+++ app/error.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h2 className="text-xl">Something went wrong!</h2>
+      <button
+        className="btn"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
